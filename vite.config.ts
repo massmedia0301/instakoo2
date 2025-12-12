e9547db19b5495dd05e054454d82800af33068fd
massmedia0301 <gunwoong0928@gmail.com>
2025-12-12 20:08:58 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are linked relatively for GitHub Pages
+
+  // GitHub Pages (project page) base path
+  // https://massmedia0301.github.io/instakoo2/
+  base: '/instakoo2/',
+
   build: {
     outDir: 'dist',
   },
